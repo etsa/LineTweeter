88ec515b309bf72c35f1da0b586d58c4a231e3ba
Mike <themikenelli@gmail.com>
2014-02-16 01:25:38 -0800

    Added Random selection to lines in lines.txt.

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import tweepy, time
+import tweepy, time, random
 
 CONSUMER_KEY = 'xxxxxxxxxxxx'
 CONSUMER_SECRET = 'xxxxxxxxxxxx' # Make sure access level is Read And Write in the Settings tab
@@ -8,11 +8,10 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-filename=open('lines.txt','r')
-f=filename.readlines()
-filename.close()
+fileobj = open('lines.txt','r')
+line = random.choice(fileobj.readlines())
+fileobj.close()
 
-for line in f:
-     api.update_status(line)
-     print line
-     time.sleep(3600) # Sleep for 1 hour
+api.update_status(line)
+print line
+time.sleep(3600) # Sleep for 1 hour
